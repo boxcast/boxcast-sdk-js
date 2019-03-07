a98b7fe0c53766ca64e968a54b80afd37e6bfe21
Mike Griffith <bikegriffith@gmail.com>
2019-03-07 16:26:58 -0500

    remove some redundant code

--- src/api/auth_broadcast_routes.js
+++ src/api/auth_broadcast_routes.js
@@ -3,27 +3,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 //
 
-const axios = require('axios');
+import { BaseAuthenticatedRoute } from './base_route';
 
-import { API_ROOT } from '../config';
-import { parseList } from '../utils';
-
-export default class AuthBroadcastRoutes {
-
-  constructor(headers) {
-    this.headers = headers;
-  }
-
-  list(params = {}) {
-    var headers = Object.assign({params}, this.headers);
-    return axios.get(`${API_ROOT}/broadcasts`, headers).then(parseList);
-  }
-
-  get(broadcastId) {
-    if (!broadcastId) {
-      return Promise.reject('broadcastId is required');
-    }
-    return axios.get(`${API_ROOT}/broadcasts/${broadcastId}`, this.headers).then((r) => r.data);
-  }
-
+export default class AuthBroadcastRoutes extends BaseAuthenticatedRoute {
+  get resourceBase() { return 'broadcasts'; }
 }
--- src/api/auth_channel_routes.js
+++ src/api/auth_channel_routes.js
@@ -3,27 +3,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 //
 
-const axios = require('axios');
+import { BaseAuthenticatedRoute } from './base_route';
 
-import { API_ROOT } from '../config';
-import { parseList } from '../utils';
-
-export default class AuthChannelRoutes {
-
-  constructor(headers) {
-    this.headers = headers;
-  }
-
-  list(params = {}) {
-    var headers = Object.assign({params}, this.headers);
-    return axios.get(`${API_ROOT}/account/channels`, headers).then(parseList);
-  }
-
-  get(channelId) {
-    if (!channelId) {
-      return Promise.reject('channelId is required');
-    }
-    return axios.get(`${API_ROOT}/channels/${channelId}`, this.headers).then((r) => r.data);
-  }
-
+export default class AuthChannelRoutes extends BaseAuthenticatedRoute {
+  get resourceBase() { return 'account/channels'; }
 }
