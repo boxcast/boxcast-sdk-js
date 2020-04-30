e2761f5b6bbccf783883dea9c26ae70bc78190e2
Mike Griffith <bikegriffith@gmail.com>
2020-04-30 07:36:47 -0400

    do not attempt to present playlist in view if playlist is not reporting live or recorded

--- src/api/view_routes.js
+++ src/api/view_routes.js
@@ -13,7 +13,16 @@ export default class ViewRoutes {
     if (!broadcastId) {
       return Promise.reject('broadcastId is required');
     }
-    return axios.get(`${API_ROOT}/broadcasts/${broadcastId}/view`, {params}).then((r) => r.data);
+    return axios.get(`${API_ROOT}/broadcasts/${broadcastId}/view`, {params}).then((r) => r.data).then((view) => {
+      if (view && view.playlist) {
+        if (view.status.indexOf('live') < 0 && view.status.indexOf('recorded') < 0) {
+          // Not yet ready; shouldn't start looking at this playlist.
+          console.log('Playlist not yet ready; status is [', view.status, '] for ', view.playlist);
+          view.playlist = '';
+        }
+      }
+      return view;
+    });
   }
 
 }
