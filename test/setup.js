f96eb6083215498a74785d3df8cbe6742dddce3f
Mike Griffith <bikegriffith@gmail.com>
2021-09-09 11:47:12 -0400

    mock storage

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,14 @@
 var html;
 var options = { url: 'http://localhost/' };
 var jsdom = require('jsdom-global')(html, options);
+
+var mockStorage = {
+  getItem: function (key) {
+    return this[key];
+  },
+  setItem: function (key, value) {
+    this[key] = value;
+  }
+};
+global.localStorage = mockStorage;
+global.sessionStorage = mockStorage;
